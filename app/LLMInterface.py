f5ddb6a34c64f26c96f4089c852f0225d3ca463a
p-Lukas <patrick@enai.io>
2024-10-08 12:52:34 +0200

    baseline docker img for test deployment in AWS

--- app/LLMInterface.py
+++ app/LLMInterface.py
@@ -10,49 +10,7 @@ class LLMInterface:
         self.mistral_client = requests.Session()
 
     def send_chat(self, question: str, context: str) -> Response:
-        response = self.mistral_client.post(
-            "https://api.mistral.ai/v1/chat/completions",
-            headers={"Authorization": f"Bearer {os.getenv('MISTRAL_API_KEY')}"},
-            json=self.build_chat_request_mistral(question, context)
-        )
-        return response
-
-    def build_chat_request_mistral(self, question: str, context: str) -> dict:
-        return {
-            "model": self.mistral_model,
-            "temperature": 0.7,
-            "top_p": 1,
-            "max_tokens": 0,
-            "min_tokens": 0,
-            "stream": False,
-            "stop": "string",
-            "random_seed": 0,
-            "messages": [
-                {
-                    "role": "system",
-                    "content": "You are a helpful assistant and have access to the following context: " + context
-                },
-                {
-                    "role": "user",
-                    "content": question
-                }
-            ],
-            "response_format": {
-                "type": "text"
-            },
-            "tools": [
-                {
-                    "type": "function",
-                    "function": {
-                        "name": "string",
-                        "description": "",
-                        "parameters": {}
-                    }
-                }
-            ],
-            "tool_choice": "auto",
-            "safe_prompt": False
-        }
+        pass
 
     def build_chat_request_gpt4o(self, question: str, context: str) -> dict:
         pass
--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from app.rest.v1 import ClientDocumentResource, AdminResource, SearchResource
 from dotenv import load_dotenv
 
 load_dotenv()
 webServer = FastAPI()
-webServer.include_router(ClientDocumentResource.router)
-webServer.include_router(AdminResource.router)
-webServer.include_router(SearchResource.router)
+# webServer.include_router(Endpoints.router)
+
+@webServer.get("/status")
+def status():
+    return {"status": "ok"}
