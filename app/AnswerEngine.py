c011afb4f95b78d885c3832090035e56b1747158
p-Lukas <patrick@enai.io>
2024-10-14 16:55:54 +0200

    switch engine to chatGPT

--- app/AnswerEngine.py
+++ app/AnswerEngine.py
@@ -8,7 +8,7 @@ class AnswerEngine:
     def get_answer(self, question: str):
         # build and send request to LLM
         print("requesting answer for question: ", question)
-        # chat_response = self.llm_interface.send_chat_to_openai(question)
-        chat_response = self.llm_interface.send_chat_to_perplexity(question)
+        chat_response = self.llm_interface.send_chat_to_openai(question)
+        # chat_response = self.llm_interface.send_chat_to_perplexity(question)
         return {"message": chat_response}
     
