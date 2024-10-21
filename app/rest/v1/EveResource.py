02888003e7fc56b916baf85711cee49514d24dae
predetermined <l@l5d.li>
2024-10-21 14:37:34 +0200

    add streaming and message context

--- app/rest/v1/EveResource.py
+++ app/rest/v1/EveResource.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from typing import Annotated
 from uuid import UUID
 from urllib.parse import unquote
-from app.AnswerEngine import AnswerEngine
+from app.AnswerEngine import AnswerEngine, MessageList
 from app.services.CheckTokenService import get_current_user
 
 router = APIRouter(
@@ -15,6 +15,16 @@ router = APIRouter(
 def answer(user_id: UUID, q: str, answer_engine: Annotated[AnswerEngine, Depends(AnswerEngine)]):
     decoded_q = unquote(q)
     return answer_engine.get_answer(decoded_q)
+
+@router.post("/{user_id}/answer-stream")
+async def answer_stream(
+    user_id: UUID,
+    q: str,
+    answer_engine: Annotated[AnswerEngine, Depends(AnswerEngine)],
+    payload: MessageList,
+):
+    decoded_q = unquote(q)
+    return answer_engine.get_answer_stream(decoded_q, messages=payload.messages)
 
 @router.get("/{user_id}/welcome-text")
 def welcome_text(user_id: UUID, sname: str, answer_engine: Annotated[AnswerEngine, Depends(AnswerEngine)]):
