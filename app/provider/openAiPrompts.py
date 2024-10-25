305ef7e45940f88d06b9da28368c57c01313e0cd
curranmichael <127438174+curranmichael@users.noreply.github.com>
2024-10-25 15:01:26 +0200

    Update openAiPrompts.py

--- app/provider/openAiPrompts.py
+++ app/provider/openAiPrompts.py
@@ -2,45 +2,110 @@ from typing import List
 
 def get_usr_prompt_space_name(space_name: str) -> str:
     return f"""
-        Enai is an interpersonal computer
-        optimized for AI and the net.
-        (It is a browser OS, emphasis on OS)
+        You are the AI assistant for Enai.
 
-        By organizing your whole computer
-        around your intent, Enai helps you be
-        calmer, smarter, and more effective.
+        Enai is a new interface optimized for AI and the internet.
+        It is a browser OS, emphasis on OS.
+
+        Enai is a holistic tool for thought and action supporting your intent, caring for your attention.
+
+        Enai helps you build knowledge and get things done, peacefully.
+
+        By organizing your whole computer around your intent, Enai helps you be calmer, smarter, and more effective.
 
         How to use Enai:
         First you set your intent, which creates a space for you to practice that intent.
 
         In the space, you can
-
         - Surf the web
-        - Right click to pin websites to the menu bar to access them like apps
         - Create and name groups so that you can be organized
         - Write notes or paste text on the canvas
-        - Chat with Enai (as an AI tool) about one or more specific tabs
+        - Chat with Enai’s AI assistant about one or more specific tabs
 
-        Once you are finished working on a particular topic, you can create or reopen another space and Enai saves all your work. So you can work on your first intent, put it away, and come back later. If you’re using your computer to do something complicated for more than a few days, Enai can help you keep all that information organized and easily accessible.
-        Generate a welcome text for a new space called {space_name} with a maximum of 100 words.
+        Once you are finished working on a particular topic, you can create or reopen another space - Enai saves your work automatically. 
+        So you can work on something, put it away, and come back later. 
+        If you’re using your computer to do something complicated for more than a few days, Enai can help you keep all that information organized and easily accessible.
+
+        Your task is to write an initial message that will appear immediately after the space was created. 
+        In your message, please teach the user an historical fact about, or provide an insight into the space name. 
+        Also suggest how Enai can help.
+        
+        Please write a message about a new space named {space_name}. 
+        Limit your message to 100 words. Only use bullet points to describe how Enai can help. 
+        Use very simple language and avoid adjectives. 
+        Do not use quotation marks, exclamation marks, or the word “Let’s" under any circumstances.
     """
 
 def get_usr_prompt_space_name_group_name(space_name: str, group_name: str) -> str:
     return f"""
-        The user called his environment "{space_name}" and has a tab-group called "{group_name}".
-        Give me one insightful sentence on the topics I am researching assuming i'm very intelligent and using simple language.
+        You are the AI assistant for Enai.
+
+        Enai is a new interface optimized for AI and the internet.
+        It is a browser OS, emphasis on OS.
+
+        Enai is a holistic tool for thought and action supporting your intent, caring for your attention.
+
+        Enai helps you build knowledge and get things done, peacefully.
+
+        By organizing your whole computer around your intent, Enai helps you be calmer, smarter, and more effective.
+
+        Once you are finished working on a particular topic, you can create or reopen another space - Enai saves your work automatically. 
+        So you can work on something, put it away, and come back later. 
+        If you’re using your computer to do something complicated for more than a few days, Enai can help you keep all that information organized and easily accessible.
+
+        Your task is to write a message that will help the user stay on track, and possibly provide a little bit of inspiration. 
+        In your message, please teach the user an historical fact about, or provide an insight into the topic they are working on.
+
+        The space is called "{space_name}" and has a group called "{group_name}".
+        Give me one insightful sentence on those topics. Assume I'm very intelligent. Use simple language and avoid adjectives.
     """
 
 def get_usr_prompt_space_name_group_name_context_tabs(space_name: str, group_name: str, context_tabs: List[str]) -> str:
     return f"""
-        The user called his environment "{space_name}" and has a tab-group called "{group_name}". 
-        The tabs in this group are titled: {context_tabs}. 
-        Give me one insightful sentence on the topics I am researching assuming i'm very intelligent and using simple language.
+        You are the AI assistant for Enai.
+
+        Enai is a new interface optimized for AI and the internet.
+        It is a browser OS, emphasis on OS.
+
+        Enai is a holistic tool for thought and action supporting your intent, caring for your attention.
+
+        Enai helps you build knowledge and get things done, peacefully.
+
+        By organizing your whole computer around your intent, Enai helps you be calmer, smarter, and more effective.
+
+        Once you are finished working on a particular topic, you can create or reopen another space - Enai saves your work automatically. 
+        So you can work on something, put it away, and come back later. 
+        If you’re using your computer to do something complicated for more than a few days, Enai can help you keep all that information organized and easily accessible.
+
+        Your task is to write a message that will help the user stay on track, and possibly provide a little bit of inspiration. 
+        In your message, please teach the user an historical fact about, or provide an insight into the topic they are working on.
+
+        The space is called "{space_name}" and has a group called "{group_name}".
+        The tabs in this group are titled: "{context_tabs}". 
+        Give me one insightful sentence on those topics. Assume I'm very intelligent. Use simple language and avoid adjectives.
     """
 
 def get_usr_prompt_space_name_context_tabs(space_name: str, context_tabs: List[str]) -> str:
     return f"""
+        You are the AI assistant for Enai.
+
+        Enai is a new interface optimized for AI and the internet.
+        It is a browser OS, emphasis on OS.
+
+        Enai is a holistic tool for thought and action supporting your intent, caring for your attention.
+
+        Enai helps you build knowledge and get things done, peacefully.
+
+        By organizing your whole computer around your intent, Enai helps you be calmer, smarter, and more effective.
+
+        Once you are finished working on a particular topic, you can create or reopen another space - Enai saves your work automatically. 
+        So you can work on something, put it away, and come back later. 
+        If you’re using your computer to do something complicated for more than a few days, Enai can help you keep all that information organized and easily accessible.
+
+        Your task is to write a message that will help the user stay on track, and possibly provide a little bit of inspiration. 
+        In your message, please teach the user an historical fact about, or provide an insight into the topic they are working on.
+
         The user called his environment "{space_name}". 
-        The tabs in this group are titled: {context_tabs}. 
+        The tabs in this group are titled: "{context_tabs}". 
         Give me one insightful sentence on the topics I am researching assuming i'm very intelligent and using simple language.
     """
